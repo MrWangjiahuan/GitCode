160aae11bd91c5e6172b33eab0ba00462d6b0515
MrWangjiahuan <mr.wangjiahuan@gmail.com>
2019-02-12 10:43:46 +0800

    Encapsulating HTTP classes

---
+++ config.js
@@ -0,0 +1,8 @@
+
+const config = {
+    api_base_url: 'https://music.wangjiahuan.top',
+}
+
+
+
+export { config }
---
+++ utils/http.js
@@ -0,0 +1,32 @@
+import { config } from "../config.js";
+// # 解构
+class HTTP {
+  request({ url, data = {}, method = "GET" }) {
+    return new Promise((resolve, reject) => {
+      this._request(url, resolve, reject, data, method);
+    });
+  }
+  _request(url, resolve, reject, data = {}, method = "GET") {
+    wx.request({
+      url: config.api_base_url + url,
+      method: method,
+      data: data,
+      header: {
+        "content-type": "application/json"
+      },
+      success: res => {
+        const code = res.statusCode.toString();
+        if (code.startsWith("2")) {
+          resolve(res.data);
+        } else {
+          reject();
+        }
+      },
+      fail: err => {
+        reject(err);
+      }
+    });
+  }
+}
+
+export { HTTP };
